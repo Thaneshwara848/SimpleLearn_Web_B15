6231754ab7e9c9c46eafa8d5ae2d3a23a177a00d
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2023-04-01 11:19:56 +0530

    binding with ex

--- src/app/eventdemo/eventdemo.component.ts
+++ src/app/eventdemo/eventdemo.component.ts
@@ -21,4 +21,30 @@ export class EventdemoComponent {
   mychoice=false;
 
   name="prakash";
+  mytotal=0;
+  myemi=0;
+  roi=0;
+
+   cal(p:any,t:any,r:any){
+   
+    this.mytotal=(p*t*r)/100;
+    this.myemi=this.mytotal/t;
+ 
+  }
+
+   loan(event:any){
+    // console.log(event.target.value)
+    if(event.target.value=='car'){
+            this.roi=8;
+    }
+    else   if(event.target.value=='home'){
+            this.roi=10;
+    }
+    else   if(event.target.value=='personal'){
+            this.roi=13;
+    }
+    else   if(event.target.value=='eduation'){
+           this.roi=11;
+    }
+ }
 }
